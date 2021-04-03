46a922abeba993c22e96e016e3aeb7c194ec04ec
Dionne Smith-Jackson <dionnesmithjackson@gmail.com>
2021-04-02 22:39:17 -0400

    Saves a note to the database

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // Setup DB
 const JSONdb = require('simple-json-db');
-const db = new JSONdb('/db/db.json');
+const db = new JSONdb('db/db.json');
+
+// Set up Unique ID
+const { v4: uuidv4 } = require('uuid');
+
 
 // Setup Express
 const express = require('express')
 const exphbs = require('express-handlebars')
+
 
 const app = express()
 
@@ -14,6 +19,13 @@ app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars');
 app.use(express.static('public'))
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -22,12 +34,28 @@ app.get('/notes', (req, res) => {
     res.render('notes');
 })
 
-app.get('/api/notes', (req,res) => {
+app.get('/api/notes', (req, res) => {
+    res.send('hi');
+});
+
+app.get('/api/notes/:id', (req, res) => {
 
 });
 
 app.post('/api/notes', (req, res) => {
+    const uuid = uuidv4();
+    console.log(req.body)
+    const title = req.body.title;
+    const body = req.body.note;
 
+    const note = {
+        title: title,
+        body: body,
+    }
+
+    db.set(uuid, JSON.stringify(note));
+
+    res.send("yay");
 });
 
 app.delete('/api/notes/:id', (req, res) => {
