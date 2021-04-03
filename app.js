0b06d5dd321fe7985d8a83ea7faefacf8d35fd87
Dionne Smith-Jackson <dionnesmithjackson@gmail.com>
2021-04-02 23:01:38 -0400

    API finished

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const JSONdb = require('simple-json-db');
 const db = new JSONdb('db/db.json');
 
 // Set up Unique ID
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, validate: uuidValidate } = require('uuid');
 
 
 // Setup Express
@@ -34,14 +34,22 @@ app.get('/notes', (req, res) => {
     res.render('notes');
 })
 
+// Get all the notes
 app.get('/api/notes', (req, res) => {
-    res.send('hi');
+    res.send(db.JSON());
 });
 
+// Read a note from the database
 app.get('/api/notes/:id', (req, res) => {
-
+    const uuid = req.params.id;
+    if (uuidValidate(uuid)) {
+        const data = JSON.parse(db.get(uuid));
+        res.send(data);
+    }
+    
 });
 
+// Saves a note to the database
 app.post('/api/notes', (req, res) => {
     const uuid = uuidv4();
     console.log(req.body)
@@ -55,10 +63,17 @@ app.post('/api/notes', (req, res) => {
 
     db.set(uuid, JSON.stringify(note));
 
-    res.send("yay");
+    res.send(uuid);
 });
 
+// Deletes a note
 app.delete('/api/notes/:id', (req, res) => {
+    const uuid = req.params.id;
+    console.log('request to delete', uuid);
+    if (uuidValidate(uuid)) {
+        const result = db.delete(uuid);
+        res.send(result);
+    }
 
 });
 
