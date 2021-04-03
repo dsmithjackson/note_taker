48feed8f25d41cdc7c7b1ab64da4afb2b5049529
Dionne Smith-Jackson <dionnesmithjackson@gmail.com>
2021-04-03 01:33:11 -0400

    Readme and ESLint

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# note_taker
+# Note Taker Application
+
+This note taking app is for keeping track of your day. It lets you write and save notes. Notes are saved in localstorage on the client side.
+
+# Screenshot
+![The App](docs/screenshot.png)
+
+
+# Link
+https://proud-flower-2776.herokuapp.com/
--- app.js
+++ app.js
@@ -1,83 +1,79 @@
 // Setup DB
 const JSONdb = require('simple-json-db');
+
 const db = new JSONdb('db/db.json');
 
 // Set up Unique ID
-const { v4: uuidv4, validate: uuidValidate } = require('uuid');
-
+const {
+  v4: uuidv4,
+  validate: uuidValidate,
+} = require('uuid');
 
 // Setup Express
-const express = require('express')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const exphbs = require('express-handlebars');
 
+const app = express();
 
-const app = express()
+const port = 3000;
 
-const port = 3000
-
-app.engine('handlebars', exphbs())
+app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+  extended: true,
 }));
 
-
-
-
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 });
 app.get('/notes', (req, res) => {
-    res.render('notes');
-})
+  res.render('notes');
+});
 
 // Get all the notes
 app.get('/api/notes', (req, res) => {
-    res.send(db.JSON());
+  res.send(db.JSON());
 });
 
 // Read a note from the database
 app.get('/api/notes/:id', (req, res) => {
-    const uuid = req.params.id;
-    if (uuidValidate(uuid)) {
-        const data = JSON.parse(db.get(uuid));
-        res.send(data);
-    }
-    
+  const uuid = req.params.id;
+  if (uuidValidate(uuid)) {
+    const data = JSON.parse(db.get(uuid));
+    res.send(data);
+  }
 });
 
 // Saves a note to the database
 app.post('/api/notes', (req, res) => {
-    const uuid = uuidv4();
-    console.log(req.body)
-    const title = req.body.title;
-    const body = req.body.body;
+  const uuid = uuidv4();
+  console.log(req.body);
+  const { title } = req.body;
+  const { body } = req.body;
 
-    const note = {
-        title: title,
-        body: body,
-    }
+  const note = {
+    title,
+    body,
+  };
 
-    db.set(uuid, JSON.stringify(note));
+  db.set(uuid, JSON.stringify(note));
 
-    res.send(uuid);
+  res.send(uuid);
 });
 
 // Deletes a note
 app.delete('/api/notes/:id', (req, res) => {
-    const uuid = req.params.id;
-    console.log('request to delete', uuid);
-    if (uuidValidate(uuid)) {
-        const result = db.delete(uuid);
-        res.send(result);
-    }
-
+  const uuid = req.params.id;
+  console.log('request to delete', uuid);
+  if (uuidValidate(uuid)) {
+    const result = db.delete(uuid);
+    res.send(result);
+  }
 });
 
-
 app.listen(process.env.PORT || port, () => {
-    console.log(`app running on ${port}`)
-})
+  console.log(`app running on ${port}`);
+});
